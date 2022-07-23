64c94d5ccc0469bbaea0c4f2113c03a0394a349e
Ade Mahendra <hendraadem@gmail.com>
2022-07-23 23:17:43 +0700

    refectored database connection

--- config/Database.js
+++ config/Database.js
@@ -1,10 +1,10 @@
 import { Sequelize } from "sequelize";
 
-const host = "localhost";
+const host = "ec2-107-22-122-106.compute-1.amazonaws.com";
 const port = 5432;
-const user = "postgres";
-const pass = "root";
-const database = "ngantin";
+const user = "uvocixkjxbqgga";
+const pass = "81a1d2a334b947529eb8fa94d2f2b018a4662c3c27e0b1a21361fa771ae50710";
+const database = "df4vmkb94m9vpk";
 
 const db = new Sequelize(database, user, pass, {
   host: host,
